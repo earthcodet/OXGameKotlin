fb6635a36eb3a9bd6f283a96abbcba4dc41e47f4
heroserth2 <59160969@go.buu.ac.th>
2019-08-01 10:38:43 +0700

    test v3

--- src/XOGame.kt
+++ src/XOGame.kt
@@ -1,39 +1,36 @@
-fun main() {
-    val table = arrayOf( arrayOf("-","-","-"),arrayOf("-","-","-"),arrayOf("-","-","-"))
-//    print("Please input Row Col : ")
-//    val input = readLine()
-//    val rowcol:List<String>? = input?.split(" ")
-//    print("input ${rowcol?.get(0)}")
-    showTable(table)
-    input()
-}
-fun input() {
-    try {
-        val input = readLine()
-        val rowcol:List<String>? = input?.split(" ")
-        if(rowcol?.size != 2){
-            print("error != 2")
-        }
-        val row = rowcol?.get(0)?.toInt()
-        val col = rowcol?.get(1)?.toInt()
-
-        val result: List<Int?> = listOf(row,col)
-        print(result)
-    }catch (e : Throwable) {
-        print("error number")
+class XOGame{
+    val row:Int = 0
+    val table:Array<Array<String>> = arrayOf( arrayOf("-","-","-"),arrayOf("-","-","-"),arrayOf("-","-","-"))
+    fun main() {
+        print("dasdas")
+        showTable(table)
+        //input()
     }
-}
-fun showError() {
-    //
-}
+//    fun input() {
+//        try {
+//            val input = readLine()
+//            val rowcol:List<String>? = input?.split(" ")
+//            if(rowcol?.size != 2){
+//                print("error != 2")
+//            }
+//            val row = rowcol?.get(0)?.toInt()
+//            val col = rowcol?.get(1)?.toInt()
+//
+//            val result: List<Int> = listOf(row,col) as List<Int>
+//        }catch (e : Throwable) {
+//          //  showError()
+//        }
+//    }
 
-fun showTable(table:Array<Array<String>>) {
-    println("  1 2 3")
-    for( i in table.indices){
-        print("${i+1} ")
-        for(j in table[i].indices){
-            print("${table[i][j]} ")
+    fun showTable(table:Array<Array<String>>) {
+        println("  1 2 3")
+        for( i in table.indices){
+            print("${i+1} ")
+            for(j in table[i].indices){
+                print("${table[i][j]} ")
+            }
+            println("")
         }
-        println("")
     }
+
 }
